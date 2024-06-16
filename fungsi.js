353ce1ea1e635fd6672a671ac7d0b59224e34237
mieramensatu <sasakihaise985@gmail.com>
2024-06-16 11:32:49 +0700

    coba conflik

--- fungsi.js
+++ fungsi.js
@@ -6,8 +6,7 @@ console.log(subtract(1, 2));
 
 const multiply = (a, b) => a * b;
 console.log(multiply(1, 2));
-
-console.log(multiply(1, 2));
+console.log(multiply(2, 2));
 
 const divide = (a, b) => a / b;
 console.log(divide(4, 2));
