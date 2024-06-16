94c2bc87951656c5925c595d50b2e7b6d75b632b
mieramensatu <sasakihaise985@gmail.com>
2024-06-16 17:13:21 +0700

    add feat: nambah console.log pada pengurangan

--- fungsi.js
+++ fungsi.js
@@ -3,6 +3,7 @@ console.log(add(1, 2));
 
 const subtract = (a, b) => a - b;
 console.log(subtract(1, 2));
+console.log(subtract(2, 2));
 
 const multiply = (a, b) => a * b;
 console.log(multiply(1, 2));
