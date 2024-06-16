9ef5d969aae05d27db3596e11dd62498c09a0778
mieramensatu <sasakihaise985@gmail.com>
2024-06-16 11:25:23 +0700

    test

--- fungsi.js
+++ fungsi.js
@@ -7,5 +7,5 @@ console.log(subtract(1, 2));
 const multiply = (a, b) => a * b;
 console.log(multiply(1, 2));
 
-const divide1 = (a, b) => a / b;
-console.log(divide1(4, 2));
+const divide = (a, b) => a / b;
+console.log(divide(4, 2));
